645357cfb474d7fafab61fa655394d96442aa161
Lukas <47306842+xjavor20@users.noreply.github.com>
2019-04-10 18:50:44 +0200

    Nepovinne funkcie (#15)
    * Gramaticka chyba
    
    * Nepovinne funkcie, oprava chyb v testoch a matematickej funkcii

--- src/math42.py
+++ src/math42.py
@@ -1,4 +1,6 @@
 import math
+import re
+
 
 def addition(x, y):
     """Vypocita sucet cisel x a y
@@ -8,67 +10,73 @@ def addition(x, y):
 
         :param x: prvy operand
         :param y: druhy operand
-    	:return: sucet dvoch operandov
+        :return: sucet dvoch operandov
     """
-  
+
     return x+y
+
 
 def subtract(x, y):
     """Vypocita rozdiel cisel x a y
-	
-	:param x: prvy operand
-	:param y: druhy operand
-	:return: rozdiel dvoch operandov
+
+        :param x: prvy operand
+        :param y: druhy operand
+        :return: rozdiel dvoch operandov
     """
-  
+
     return x-y
+
 
 def multiplication(x, y):
     """Vypocita sucin dvoch cisel x a y
-	
-	:param x: prvy cinitel
-	:param y: druhy cinitel
-	:return: sucin dvoch cinitelov
+
+        :param x: prvy cinitel
+        :param y: druhy cinitel
+        :return: sucin dvoch cinitelov
     """
-  
+
     return x*y
+
 
 def division(x, y):
     """Vypocita podiel dvoch cisel x a y
-	
-	:param x: delenec
-	:param y: delitel
-	:return: podiel delenca a delitela
+
+        :param x: delenec
+        :param y: delitel
+        :return: podiel delenca a delitela
     """
     if y == 0:
         raise ValueError('Delenie nulou nie je povolene!')
-	
+
     return float(x)/y
+
 
 def factorial(x):
     """Vypocita factorial cisla x
-	
-	:param x: cislo pre vypocet faktorialu
-	:return: hodnota faktorialu
+
+        :param x: cislo pre vypocet faktorialu
+        :return: hodnota faktorialu
     """
-    
+
     return math.factorial(x)
+
 
 def absoluteNumber(x):
     """Vypocita absolutnu hodnotu zadaneho cisla
-	:param x: cislo pre vypocet absolutnej hodnoty
-	:return: cislo x vratene v absolutnej hodnote
+        :param x: cislo pre vypocet absolutnej hodnoty
+        :return: cislo x vratene v absolutnej hodnote
     """
 
     if x < 0:
-	return x*(-1)
+        return x*(-1)
 
     return x
 
+
 def naturalExponent(x):
     """Vypocita pre cislo x funkciu e^x
-	:param x: cislo pre vypocet e^x
-	:return: vysledok danej funkcie pre x
+        :param x: cislo pre vypocet e^x
+        :return: vysledok danej funkcie pre x
     """
 
     return math.exp(x)
@@ -76,11 +84,86 @@ def naturalExponent(x):
 
 def ln(x):
     """Vypocita prirodzeny logaritmus zadaneho cisla
-	:param x: cislo pre vypocet prir. logaritmu
-	:return: vrati vysledok pre prir. logaritmus zadaneho cisla
+        :param x: cislo pre vypocet prir. logaritmu
+        :return: vrati vysledok pre prir. logaritmus zadaneho cisla
     """
 
     if x <= 0:
-	raise ValueError('Zadane cislo musi byt vacsie ako 0!')
+        raise ValueError('Zadane cislo musi byt vacsie ako 0!')
 
     return math.log(x)
+
+
+def sqrt(x):
+    """Vypocita obecnu odmocninu (druhu odmocninu) z cisla x
+        :param x: cislo pre vypocet obecnej odmocniny
+        :return: Obecna odmocnina cisla x
+    """
+
+    if x < 0:
+        raise ValueError('Zadane cislo musi byt vacsie alebo rovne 0!')
+
+    return math.sqrt(x)
+
+
+def root(x, y):
+    """Vypocita y-ntu odmocninu z cisla x
+        :param x: cislo z ktoreho chceme odmocninu
+        :param y: kolka odmocnina
+        :return: Y-nta odmocnina z x
+    """
+
+    if x < 0:
+        raise ValueError('Zadane cislo "X" musi byt vacsie alebo rovne 0!')
+
+    if y <= 0:
+        raise ValueError('Zadane cislo "Y" musi byt vacsie ako 0!')
+
+    return math.pow(x, (1/y))
+
+
+def binToDec(xb):
+    """Premeni binarne cislo na dekadicke
+        :param xb: binarne cislo
+        :return: Dekadicke cislo z binarneho vstupu
+    """
+
+    is_bin = re.match('^[01]+$', xb)
+
+    if is_bin:
+        binary_num = int(xb, 2)
+        return binary_num
+    else:
+        raise ValueError('Zadane cislo musi byt zadane v binarnom tvare!')
+
+
+def hextoDec(xh):
+    """Premeni hexadecimalne cislo na dekadicke
+        :param xh: hexadecimalne cislo
+        :return: Dekadicke cislo z hexadecimalneho vstupu
+    """
+
+    is_hex = re.match('^[0-9A-F]+$', xh, re.IGNORECASE)
+
+    if is_hex:
+        hex_num = int(xh, 16)
+        return hex_num
+    else:
+        raise ValueError(
+            'Zadane cislo musi byt zadane v hexadecimalnom tvare!')
+
+
+def octToDec(xo):
+    """Premeni oktalove cislo na dekadicke
+        :param xo: oktalove cislo
+        :return: Dekadicke cislo z oktaloveho vstupu
+    """
+
+    is_oct = re.match('^[0-7]+$', xo)
+
+    if is_oct:
+        oct_num = int(xo, 8)
+        return oct_num
+    else:
+        raise ValueError(
+            'Zadane cislo musi byt zadane v oktalovom tvare!')
--- tests/math_tests/basic_operations_test.py
+++ tests/math_tests/basic_operations_test.py
@@ -20,7 +20,7 @@ class BasicOperations(unittest.TestCase):
     def test_division(self):
         self.assertEqual(math.division(36, 6), 6)
         self.assertAlmostEqual(math.division(9, 4), 2.25)
-        self.assertRaises(ValueError,math.division,42,0)
+        self.assertRaises(ValueError, math.division, 42, 0)
 
 
 class AdvancedOperations(unittest.TestCase):
@@ -29,9 +29,9 @@ class AdvancedOperations(unittest.TestCase):
         self.assertEqual(math.factorial(9), 362880)
 
     def test_abs(self):
-        self.assertEqual(abs(-9), 9)
-        self.assertEqual(abs(0), 0)
-        self.assertEqual(abs(9), 9)
+        self.assertEqual(math.absoluteNumber(-9), 9)
+        self.assertEqual(math.absoluteNumber(0), 0)
+        self.assertEqual(math.absoluteNumber(9), 9)
 
     def test_naturalExponent(self):
         self.assertEqual(math.naturalExponent(0), 1)
@@ -39,9 +39,48 @@ class AdvancedOperations(unittest.TestCase):
 
     def test_ln(self):
         self.assertEqual(math.ln(1), 0)
-        self.assertAlmostEqual(math.ln(403.428793493),6)
-        self.assertRaises(ValueError, math.ln,0)
-        self.assertRaises(ValueError, math.ln,-5)
+        self.assertAlmostEqual(math.ln(403.428793493), 6)
+        self.assertRaises(ValueError, math.ln, 0)
+        self.assertRaises(ValueError, math.ln, -5)
+
+    def test_sqrt(self):
+        self.assertEqual(math.sqrt(1), 1)
+        self.assertEqual(math.sqrt(0), 0)
+        self.assertEqual(math.sqrt(4), 2)
+        self.assertRaises(ValueError, math.sqrt, -2)
+
+    def test_universal_root(self):
+        self.assertEqual(math.root(8, 3), 2)
+        self.assertEqual(math.root(16, 4), 2)
+        self.assertRaises(ValueError, math.root, 0, 0)
+        self.assertRaises(ValueError, math.root, -1, 1)
+        self.assertRaises(ValueError, math.root, 5, -1)
+
+
+class OptionalOperations(unittest.TestCase):
+
+    def test_bin_to_dec(self):
+        self.assertEqual(math.binToDec("100"), 4)
+        self.assertEqual(math.binToDec("000"), 0)
+        self.assertEqual(math.binToDec("1010"), 10)
+        self.assertRaises(ValueError, math.binToDec, "120")
+        self.assertRaises(ValueError, math.binToDec, "100b")
+        self.assertRaises(ValueError, math.binToDec, "-100")
+
+    def test_hex_to_dec(self):
+        self.assertEqual(math.hextoDec("a"), 10)
+        self.assertEqual(math.hextoDec("A"), 10)
+        self.assertEqual(math.hextoDec("42"), 66)
+        self.assertRaises(ValueError, math.hextoDec, "ahoj")
+        self.assertRaises(ValueError, math.hextoDec, "-5")
+
+    def test_oct_to_dec(self):
+        self.assertEqual(math.octToDec("7"), 7)
+        self.assertEqual(math.octToDec("000"), 0)
+        self.assertEqual(math.octToDec("21"), 17)
+        self.assertRaises(ValueError, math.octToDec, "820")
+        self.assertRaises(ValueError, math.octToDec, "10d")
+        self.assertRaises(ValueError, math.octToDec, "-214")
 
 
 if __name__ == '__main__':
